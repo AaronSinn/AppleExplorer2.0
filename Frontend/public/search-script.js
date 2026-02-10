078c23db5ac81cbf08c6a5b7d84c811fb426af57
Kevin Bui <bui81@uwindsor.ca>
2026-02-10 14:51:26 -0500

    new change

--- Frontend/public/search-script.js
+++ Frontend/public/search-script.js
@@ -99,7 +99,8 @@ class SearchListManager{
     async loadApples(){
         console.log("Loading apple data...")
         try{
-            const response = await fetch("http://localhost:3000/apples");
+            const response = await fetch("https://appleexplorer2-0backend.onrender.com
+/apples");
             this.data = await response.json();
             this.filteredData = [...this.data];
             console.log(`Loaded data for ${this.data.length} apples`)
@@ -958,7 +959,8 @@ class SearchListManager{
             for(const record of records){
                 const index = records.indexOf(record);
                 const mapin = recordNames[index];
-                const response = await fetch(`http://localhost:3000/${this.mapObjectToFetchID.get(mapin)}`, {
+                const response = await fetch(`https://appleexplorer2-0backend.onrender.com
+/${this.mapObjectToFetchID.get(mapin)}`, {
                     method: 'POST',
                     headers: {
                     'Content-Type': 'application/json',
@@ -980,7 +982,8 @@ class SearchListManager{
                 data[this.mapObjectToAttributeID.get(mapin)] = id;
             }
 
-            const response = await fetch('http://localhost:3000/apples', {
+            const response = await fetch('https://appleexplorer2-0backend.onrender.com
+/apples', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
@@ -1039,7 +1042,8 @@ class SearchListManager{
             records.push(data[recordName]);
         });
         try{
-            const response = await fetch(`http://localhost:3000/apples/${data._id}`, {
+            const response = await fetch(`https://appleexplorer2-0backend.onrender.com
+/apples/${data._id}`, {
                 method: 'PUT',
                 headers: {
                 'Content-Type': 'application/json',
@@ -1072,7 +1076,8 @@ class SearchListManager{
     async deleteEntry(item){
         const data = this.lastSelectedItem;
         try{
-            const response = await fetch(`http://localhost:3000/apples/${data._id}`, {
+            const response = await fetch(`https://appleexplorer2-0backend.onrender.com
+/apples/${data._id}`, {
                 method: 'DELETE',
                 headers: {
                 'Content-Type': 'application/json',
